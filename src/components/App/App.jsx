0e73cdb5c6fcfe4bbd8599d6a90be34d15da8ccd
Abdirahman Jama <abdirahman.jama13@gmai.com>
2023-11-12 19:11:15 -0600

    header exported and imported to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header () {
+    return(
+        <div id='header-div'>
+            <h1 id='title'>Prime Pizza</h1>
+            <Cart id='Cart'/>
+        </div>
+    )
+}
+
+export default Header;
