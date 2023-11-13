ce4b54dc5e501d0bd731aa13a0b2d8ef843a097b
Robin Raabe <robinnraabe@gmail.com>
2023-11-12 20:13:40 -0600

    added CustomerInfo component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,13 +6,14 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      <Router>
+        <header className='App-header'>
+          <h1 className='App-title'>Prime Pizza</h1>
+        </header>
+    
+        <img src='images/pizza_photo.png' />
+        <p>Pizza is great.</p>
+      </Router>
     </div>
   );
 }
