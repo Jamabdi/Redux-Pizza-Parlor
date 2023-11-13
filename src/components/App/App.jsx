97e4c046e3fe61dfe5ffeb54fcba6c0fe70d2795
Stephon Tonge <stephon.tonge75@gmail.com>
2023-11-12 20:26:04 -0600

    Checkout start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,19 +3,23 @@ import axios from 'axios';
 import './App.css';
 // import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Checkout from '../Checkout/Checkout.jsx';
 
 function App() {
 
   return (
-    <div className='App'>
+    <><div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+
     </div>
+    
+    
+    </>
   );
 }
 
